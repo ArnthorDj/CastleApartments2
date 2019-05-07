b373aaf65c4f00634b1f323f080bdf6bd884a7f6
Your Name <you@example.com>
2019-05-07 10:51:33 +0000

    Clean Master

---
+++ Castle_Apartments/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Hr_School\Semester2\3ViknaVerkefni\Castle_Apartments\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
